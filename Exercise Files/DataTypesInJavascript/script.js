b59d8403c648813c611588e185a1e943b2372aa6
dbpatnodee <dbpatnode@hotmail.com>
2021-01-19 14:11:17 -0800

    added comments to data types explaining what each was.

---
+++ Exercise Files/DataTypesInJavascript/script.js
@@ -0,0 +1,21 @@
+// in the console type console.log(typeof *VariableName*) to see what type data it is.
+
+// Number: no such thing as floats in JS, any number decimal or not is a considered a number type data
+var negInteger = -3.14159265359;
+
+// String: anything wrapped with quotes single or double) this example is showing how to use backslashes to 
+// cancel out the inner quotes and still render the quotes.
+var escQuote = "Quotes can also be \"escaped\"."
+
+// Boolean: returns either true or false values
+var theSunIsWarm = true;
+
+// Null: when the value is set to null, treated like an object but is not!
+
+var emptyInside = null;
+
+// Undefined: when the vairable is undefined
+var justAnotherVariable;
+
+// Try this in your console:
+// console.log(typeof insertVariableName);
