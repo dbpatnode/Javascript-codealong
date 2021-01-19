ef2f759fa332f84ec397ffa6ef38d97482bef16a
dbpatnodee <dbpatnode@hotmail.com>
2021-01-19 12:45:48 -0800

    creating and using variables

---
+++ Exercise Files/Creating and using variables/script.js
@@ -0,0 +1,20 @@
+// to play with how this works open up the console in browser and through in a b or sum
+
+
+// you can create individual variables like so:
+
+var a = 13;
+var b = 8;
+var sum = a + b;
+
+// or create a single variable like so which (commas denot different variables ) acheives the same thing:
+
+// var a, b, sum; 
+// a = 22;
+// b = 7;
+// sum = a + b;
+
+
+// or create them all at once (which is much harder to read with a whole bunch of variables)
+
+// var a = 5,  b = 4, sum = a + b;
